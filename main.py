9a1080c10b89ae5015b3c2eaee29a41635c8adcf
wuheison <wuheison3@hotmail.com>
2024-03-20 20:58:30 +0800

    feat: can read pdf file

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import streamlit as st
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_community.llms import HuggingFaceEndpoint
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 import os
+import tempfile
 
 load_dotenv()
 HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
@@ -16,3 +20,24 @@ st.title('Chat with Files')
 uploaded_file = st.file_uploader('Upload a PDF document', type='pdf')
 # Query text
 query_text = st.text_input('Enter your question:', placeholder='Ask me anything about the document.', disabled=not uploaded_file)
+
+if uploaded_file is not None:
+    # Create a temporary file to save the uploaded file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmpfile:
+        # Write the content of the uploaded file to the temporary file
+        tmpfile.write(uploaded_file.getvalue())
+        tmpfile_path = tmpfile.name
+
+    # Initialize the PyPDFLoader with the uploaded file
+    loader = PyPDFLoader(tmpfile_path)
+
+
+   
+    documents = loader.load()
+    #splitting the text into
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    texts = text_splitter.split_documents(documents)
+    
+    st.write(texts)
+
+    os.remove(tmpfile_path)
