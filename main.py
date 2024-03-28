27e94a80bceb8871f2c44ccbfec4a8024ec94f23
wuheison <wuheison3@hotmail.com>
2024-03-29 00:06:28 +0800

    feat: add llm chatbox function.

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import tempfile
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_community.vectorstores import Chroma
-from langchain.chains import RetrievalQA
+from langchain.chains import LLMChain
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.prompts import PromptTemplate 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
@@ -13,57 +14,47 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 load_dotenv()
 HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
+# question template
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+question = PromptTemplate(template=template, input_variables=["question"])
+
+# Huggingface LLM
+repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+llm = HuggingFaceEndpoint(repo_id=repo_id, max_length=64, temperature=0.1, token=HUGGINGFACEHUB_API_TOKEN)
+
+llm_chain = LLMChain(prompt=question, 
+                     llm=llm)
+
+
 # Streamlit UI setup
 st.set_page_config(page_title='Chat with Files')
 st.title('Chat with Files')
 
-# Initialize or get the conversation history
-if 'history' not in st.session_state:
-    st.session_state.history = []
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-uploaded_file = st.file_uploader('Upload a PDF document', type='pdf')
+# Display chat messages from history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-# Function to handle the chat interaction
-def handle_chat():
-    if 'llm_response' in st.session_state and st.session_state.llm_response:
-        # Append the user's question and LLM's response to the history
-        st.session_state.history.append(('You', query_text))
-        st.session_state.history.append(('Bot', st.session_state.llm_response))
-        st.session_state.llm_response = ''  # Reset the LLM response for the next interaction
+# Accept user input
+if question := st.chat_input("Type your message here..."):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": question})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(question)
 
-# Display the conversation history
-for speaker, text in st.session_state.history:
-    st.text_area(label=speaker, value=text, height=75, disabled=True)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        with st.spinner('Thinking...'):
+            llm_response = llm_chain.run(question)
+            response = llm_response  # Assuming llm_response is the text response you want to display
 
-# Input for new questions
-query_text = st.text_input('Enter your question:', key='query_text')
-
-# Button to submit the question
-submit_button = st.button('Submit', on_click=handle_chat)
-
-if uploaded_file is not None and query_text:
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmpfile:
-        tmpfile.write(uploaded_file.getvalue())
-        tmpfile_path = tmpfile.name
-
-    # Document loading and text processing
-    loader = PyPDFLoader(tmpfile_path)
-    documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    texts = text_splitter.split_documents(documents)
-
-    # Embeddings and vector database setup
-    embedding_model_name = "mixedbread-ai/mxbai-embed-large-v1"
-    embedding = HuggingFaceEmbeddings(model_name=embedding_model_name)
-    persist_directory = 'db'
-    vectordb = Chroma.from_documents(documents=texts, embedding=embedding, persist_directory=persist_directory)
-    retriever = vectordb.as_retriever()
-    vectordb.persist()
-
-    # LLM setup and query processing
-    repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
-    llm = HuggingFaceEndpoint(repo_id=repo_id, max_length=512, temperature=0.1, token=HUGGINGFACEHUB_API_TOKEN)
-    qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
-    st.session_state.llm_response = qa_chain.invoke(query_text)
-
-    os.remove(tmpfile_path)
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
